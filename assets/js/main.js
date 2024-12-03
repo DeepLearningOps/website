5e04ece0274a393e7b0d6422b3ee6b03a322f890
Dana Engebretson <dana.engebretson@gmail.com>
2024-12-03 05:57:28 -0800

    changed images

--- assets/js/main.js
+++ assets/js/main.js
@@ -38,10 +38,11 @@
 					// Images (in the format of 'url': 'alignment').
 						images: {
 							'images/bg01.jpg': 'center',
+							'images/bg08.jpg': 'center',
 							'images/bg02.jpg': 'center',
-							'images/bg05.jpg': 'center',
+							'images/bg06.jpg': 'center',
 							'images/bg03.jpg': 'center',
-							'images/bg04.jpg': 'center'
+							'images/bg07.jpg': 'center'
 						},
 
 					// Delay.
